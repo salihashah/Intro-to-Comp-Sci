0200c4d9b858682ccdd3606cde405ccc8e5e1040
sshah3 <sshah3@HS-D210-09.MOHS.local>
2019-03-01 13:24:20 -0500

    lesson3

--- Lesson 3 Strings/src/lesson3point2strings.java
+++ Lesson 3 Strings/src/lesson3point2strings.java
@@ -5,7 +5,11 @@ public class lesson3point2strings {
 
 		
 		
-		System.out.println("a\\)
+		System.out.println("a\\b\\c\"d\"");
+		System.out.println("\\t is a tab, \\n is a new line, \\\" prints a \"quote\"");
+		System.out.println("\"line one\n \\line \t two"\\");
+
+		
 		
 		
 		
