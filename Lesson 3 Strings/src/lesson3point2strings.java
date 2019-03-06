0dcd61965d7bdb9a9fc3a133045353a940f9af7f
sshah3 <sshah3@HS-D210-09.MOHS.local>
2019-03-06 14:18:22 -0500

    Lesson3.2

--- Lesson 3 Strings/src/lesson3point2strings.java
+++ Lesson 3 Strings/src/lesson3point2strings.java
@@ -6,15 +6,23 @@ public class lesson3point2strings {
 		
 		
 		System.out.println("a\\b\\c\"d\"");
+		
 		System.out.println("\\t is a tab, \\n is a new line, \\\" prints a \"quote\"");
+		
 		System.out.println("\"line one\n \\line \t two\\");
+		
 		System.out.println("\\\\denotes a single line comment\n\\*denotes a multiline comment*\\"); 
 		
-		
-		
-		
+		System.out.println("\"\\\\\"\\\"\"\n\t\\\"\\\"\"\"\"\\\n\\\"\"\"\"\\");
 		
 		System.out.println("Birthday".toUpperCase());
+		
+		String word="it's my party";
+		System.out.println(word.substring(0,4)+" "+ word.substring(9,10)+" "+"\""+ word.substring(8,13).toUpperCase()+"\"");
+		
+		String word1="whose a whats it";
+		System.out.println(word1.substring(0,6).toUpperCase()+"/n""+"is"+word1.substring(8,12).toUpperCase());
+				
 		
 		
 		
