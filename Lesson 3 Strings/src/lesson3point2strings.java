59d844263ad4a478e73761e88b6dab342f12ccb5
sshah3 <sshah3@HS-D110-05.MOHS.local>
2019-03-06 13:17:15 -0500

    lesson3

--- Lesson 3 Strings/src/lesson3point2strings.java
+++ Lesson 3 Strings/src/lesson3point2strings.java
@@ -7,11 +7,20 @@ public class lesson3point2strings {
 		
 		System.out.println("a\\b\\c\"d\"");
 		System.out.println("\\t is a tab, \\n is a new line, \\\" prints a \"quote\"");
-		System.out.println("\"line one\n \\line \t two"\\");
+		System.out.println("\"line one\n \\line \t two\\");
+		System.out.println("\\\\denotes a single line comment\n\\*denotes a multiline comment*\\"); 
+		
+		
+		
+		
+		
+		System.out.println("Birthday".toUpperCase());
+		
+		
+		
+		
+	
 
-		
-		
-		
 		
 		
 		
