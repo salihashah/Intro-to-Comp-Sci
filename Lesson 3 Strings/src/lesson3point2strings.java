86bef1dca25efa9bdc281712e8fecfd3a7caace6
sshah3 <sshah3@HS-D210-09.MOHS.local>
2019-03-07 13:22:38 -0500

    Lesson4

--- Lesson 3 Strings/src/lesson3point2strings.java
+++ Lesson 3 Strings/src/lesson3point2strings.java
@@ -27,7 +27,7 @@ public class lesson3point2strings {
 		System.out.println(word2.substring(0,6).toLowerCase()+ word2.substring(6,10).toUpperCase()+word2.substring(10,14).toLowerCase()+"\\"+word2.substring(14,18).toUpperCase()+"\\"+ word2.substring(18,24).toLowerCase()+" "+4);
 				
 		String word3= "Peter Piper Picked A Peck Of Pickled Peppers";
-		System.out.println();
+		System.out.println(word3.substring(19,22)+word3.substring(22,36).toLowerCase()+"           "+word3.substring(37,44).toLowerCase()+" "+word3.substring(0,12)+"      "+word3.substring(29,36).toLowerCase());
 		
 		
 		
